d398c18b09348da3e58a2b99d1455fe57210b1e1
NataEf11 <melman6991@yandex.ru>
2025-04-13 11:26:13 +0200

    Update UserController.java
    удалила лишние пробелы

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -13,43 +13,43 @@ import java.util.List;
 @RequestMapping("/")
 public class UserController {
 
-	private final UserService userService;
+    private final UserService userService;
 
-	@Autowired
-	public UserController(UserService userService) {
-		this.userService = userService;
-	}
+    @Autowired
+    public UserController(UserService userService) {
+	this.userService = userService;
+    }
 
-	@GetMapping("/")
-	public String redirectToUsers() {
-		return "redirect:/users";
-	}
+    @GetMapping("/")
+    public String redirectToUsers() {
+	return "redirect:/users";
+    }
 
-	@GetMapping("/users")
-	public String getAllUsers(ModelMap model) {
-		List<User> users = userService.getAllUsers();
-		model.addAttribute("table", users);
-		return "users";
-	}
+    @GetMapping("/users")
+    public String getAllUsers(ModelMap model) {
+	List<User> users = userService.getAllUsers();
+	model.addAttribute("table", users);
+	return "users";
+    }
 
-	@PostMapping("/addUser")
-	public String addUser(@ModelAttribute User user) {
-		userService.saveUser(user);
-		return "redirect:/users";
-	}
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute User user) {
+	userService.saveUser(user);
+	return "redirect:/users";
+    }
 
-	@PostMapping("/updateUser")
-	public String updateUser(@ModelAttribute User user) {
-		User existingUser = userService.getUserById(user.getId());
-		if (existingUser != null) {
-			userService.updateUser(user);
-		}
-		return "redirect:/users";
+    @PostMapping("/updateUser")
+    public String updateUser(@ModelAttribute User user) {
+	User existingUser = userService.getUserById(user.getId());
+	if (existingUser != null) {
+	    userService.updateUser(user);
 	}
+	return "redirect:/users";
+    }
 
-	@PostMapping("/deleteUser/{id}")
-	public String deleteUser(@PathVariable Long id) {
-		userService.deleteUser(id);
-		return "redirect:/users";
-	}
+    @PostMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable Long id) {
+	userService.deleteUser(id);
+	return "redirect:/users";
+    }
 }
